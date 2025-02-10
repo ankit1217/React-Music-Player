e814805fe5ce997d382aef6f68c36cbbe5ceb2fb
ankit1217 <1217ankitraj@gmail.com>
2025-02-10 23:26:01 +0530

    pull changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import data from "./util";
 import Library from "./components/Library";
 import Nav from "./components/Nav";
 function App() {
+  // this is for pull request
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
   const [isPlaying, setIsPlaying] = useState(false);
