caaf38310bc5794fe41e49057381e5d7a885c5a0
Ankit Raj <Ankit.Raj.18@ibm.com>
2025-02-13 22:59:56 +0530

    This is due to rebase

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import data from "./util";
 import Library from "./components/Library";
 import Nav from "./components/Nav";
 function App() {
-  // this is pushed by github
+  // this is pushed by vscode
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
   const [isPlaying, setIsPlaying] = useState(false);
