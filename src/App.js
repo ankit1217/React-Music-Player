894a6c2ae8c5547db789a4d613d195ca521e1b93
ankit1217 <1217ankitraj@gmail.com>
2025-02-13 22:56:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import data from "./util";
 import Library from "./components/Library";
 import Nav from "./components/Nav";
 function App() {
+  // this is pushed by github
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
   const [isPlaying, setIsPlaying] = useState(false);
